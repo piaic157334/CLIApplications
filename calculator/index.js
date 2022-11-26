496ca7d43ec4f0a578991f39f4c0656c94e113a0
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-11-26 23:04:57 +0500

    application name printed

--- calculator/index.js
+++
@@ -1,7 +0,0 @@
-import chalk from "chalk";
-console.log(chalk.blue("     ###         ##          ##         ###         ##     ##  ###########      ###        #######"));
-console.log(chalk.blue("  ##     ##    ## ##        ##       ##     ##     ##     ##       ##        ##     ##      ##     ##"));
-console.log(chalk.blue(" ##           ##  ##       ##       ##            ##     ##       ##        ##      ##     ##   ##"));
-console.log(chalk.blue("##           #######      ##       ##            ##     ##       ##        ##      ##     #####"));
-console.log(chalk.blue(" ##    ##   ##    ##     ##         ##    ##     ##    ##       ##          ##    ##     ##  ##   #"));
-console.log(chalk.blue("    ##     ##        ##  ########      ##          ###         ##             ###       ##     ##"));
