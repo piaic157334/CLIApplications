2925d3c4c061b7af55596d3b1451c31abb8e2d0b
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-11-30 23:13:07 +0500

    basic working completed with round + and basic validation

--- number_guessing_game/output/index.js
+++ number_guessing_game/output/index.js
@@ -1,24 +1,29 @@
+import chalk from "chalk";
 import inquirer from "inquirer";
-// Variable Declarations
-let userGuess, numberToBeGuessed, numberToBeGuessedExtension = "1", score = -1, stage = 0;
-//   Logic to increase the difficulty level of the game
-for (let i = 0; i < stage; i++) {
-    numberToBeGuessedExtension = numberToBeGuessedExtension.concat("0");
-    console.log(numberToBeGuessedExtension);
-}
-numberToBeGuessed = Math.random() * parseInt(numberToBeGuessedExtension);
-numberToBeGuessed = Number(numberToBeGuessed.toFixed());
+let userGuess = {}, numberToBeGuessed = 0, numberToBeGuessedExtension, score = -1, stage = 0;
 const askANumber = async () => {
-    userGuess = await inquirer.prompt([
-        {
-            name: "userGuessedNumber",
-            type: "number",
-            message: `Guess a number between `,
-        },
-    ]);
-    console.log(userGuess);
+    //   Logic to increase the difficulty level of the game
+    numberToBeGuessedExtension = "1";
+    for (let i = 0; i < stage; i++) {
+        numberToBeGuessedExtension = numberToBeGuessedExtension.concat("0");
+    }
+    numberToBeGuessed = Math.random() * Number(numberToBeGuessedExtension);
+    numberToBeGuessed = Number(numberToBeGuessed.toFixed());
+    console.log("The number to be Guessed is 👉🏻👉🏻", numberToBeGuessed);
+    // Asking user the number to be guessed
+    do {
+        userGuess = await inquirer.prompt([
+            {
+                name: "userGuessedNumber",
+                type: "number",
+                message: `Guess a number or write 'h' for hint `,
+            },
+        ]);
+    } while (isNaN(Number(userGuess.userGuessedNumber)));
 };
+//LOgic to increase level and score if the user guesses the right number
 do {
     stage++, score++;
-    askANumber();
-} while (userGuess === null || userGuess === void 0 ? void 0 : userGuess.userGuessedNumber);
+    console.log(chalk.yellow("Stage: ", stage, "\t", "Score: ", score));
+    await askANumber();
+} while ((userGuess === null || userGuess === void 0 ? void 0 : userGuess.userGuessedNumber) === numberToBeGuessed);
--- number_guessing_game/src/index.ts
+++ number_guessing_game/src/index.ts
@@ -1,31 +1,37 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 // Variable Declarations
-
-let userGuess,
-  numberToBeGuessed: number,
-  numberToBeGuessedExtension: number | string = "1",
+type UserGuess = {
+  userGuessedNumber?: number | string;
+};
+let userGuess: UserGuess = {},
+  numberToBeGuessed: number = 0,
+  numberToBeGuessedExtension: string,
   score: number = -1,
   stage: number = 0;
-//   Logic to increase the difficulty level of the game
-for (let i = 0; i < stage; i++) {
-  numberToBeGuessedExtension = numberToBeGuessedExtension.concat("0");
-  console.log(numberToBeGuessedExtension);
-}
-numberToBeGuessed = Math.random() * parseInt(numberToBeGuessedExtension);
-numberToBeGuessed = Number(numberToBeGuessed.toFixed());
-
 const askANumber = async () => {
-  userGuess = await inquirer.prompt([
-    {
-      name: "userGuessedNumber",
-      type: "number",
-      message: `Guess a number between `,
-    },
-  ]);
-  console.log(userGuess);
+  //   Logic to increase the difficulty level of the game
+  numberToBeGuessedExtension = "1";
+  for (let i = 0; i < stage; i++) {
+    numberToBeGuessedExtension = numberToBeGuessedExtension.concat("0");
+  }
+  numberToBeGuessed = Math.random() * Number(numberToBeGuessedExtension);
+  numberToBeGuessed = Number(numberToBeGuessed.toFixed());
+  console.log("The number to be Guessed is 👉🏻👉🏻", numberToBeGuessed);
+  // Asking user the number to be guessed
+  do {
+    userGuess = await inquirer.prompt([
+      {
+        name: "userGuessedNumber",
+        type: "number",
+        message: `Guess a number or write 'h' for hint :`,
+      },
+    ]);
+  } while (isNaN(Number(userGuess.userGuessedNumber)));
 };
+//Logic to increase level and score if the user guesses the right number
 do {
   stage++, score++;
-  askANumber();
-} while (userGuess);
+  console.log(chalk.yellow("Stage: ", stage, "\t", "Score: ", score));
+  await askANumber();
+} while (userGuess?.userGuessedNumber === numberToBeGuessed);
