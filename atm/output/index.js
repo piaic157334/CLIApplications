72927bc74817ddf4cc1526bd974d792ffffac5f5
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-12-01 18:32:37 +0500

    final touches done

--- atm/output/index.js
+++ atm/output/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import chalk from "chalk";
 import inquirer from "inquirer";
+// dummy data and other variable initialization
 let bankRecord = [
     { username: "ziaKhan", pin: "1234", balance: 122000000 },
     { username: "daniyalNagori", pin: "1234", balance: 14000000 },
@@ -10,6 +11,15 @@ let bankRecord = [
     { username: "jawadAhmad", pin: "1234", balance: 187000000 },
 ];
 let userInfo, user = { username: "", pin: "", balance: 0 }, amountToBeWithDraw;
+//   printing welcome message
+console.log("  ......  ...   ....  ...........  ......       .......     .......    .....       .....  ............  ");
+console.log("  .....  .....  ..... ..........   .....      ..........   ..........  ......     ......  .......... ");
+console.log("    ...  ......  ...  ...          ...       ....     ... ...      ... ......     ......  ...  ");
+console.log("    ... ... ... ...   ........     ...      ....          ...      ... ... ...   ... ...  ......... ");
+console.log("     .....  .....     ...          ...       .....     .. ...     ...  ...   .....   ...  ...  ");
+console.log("     .....   .....    ..........   .......... ..........   ..........  ....  .....  ....  .......... ");
+console.log("      ...     ...     ...........  ..........   ........    ........   ...... ...  .....  ........... ");
+//function to ask user info
 const atm = async () => {
     do {
         userInfo = await inquirer.prompt([
@@ -24,13 +34,13 @@ const atm = async () => {
                 message: "Enter your 4 digit pin: ",
             },
         ]);
-        console.log(typeof (userInfo === null || userInfo === void 0 ? void 0 : userInfo.pin));
     } while ((userInfo === null || userInfo === void 0 ? void 0 : userInfo.pin.length) !== 4 || (userInfo === null || userInfo === void 0 ? void 0 : userInfo.username.length) < 3);
-    console.log(userInfo);
+    //   checking user's credentials if it is true or not
     if (bankRecord.find((data) => data.username === userInfo.username)) {
         if (bankRecord.find((data) => data.pin === userInfo.pin)) {
             user = bankRecord.find((data) => data.username === userInfo.username);
             console.log("Your account balance is", chalk.yellow(user === null || user === void 0 ? void 0 : user.balance));
+            //   asking to how much money customer wants to with draw
             do {
                 amountToBeWithDraw = await inquirer.prompt([
                     {
@@ -40,9 +50,13 @@ const atm = async () => {
                     },
                 ]);
             } while (isNaN(Number(amountToBeWithDraw === null || amountToBeWithDraw === void 0 ? void 0 : amountToBeWithDraw.amount)));
+            //checking if the user has sufficient balance or not
             if (user === null || user === void 0 ? void 0 : user.balance) {
-                if (user.balance > Number(amountToBeWithDraw.amount)) {
+                if (user.balance >= Number(amountToBeWithDraw.amount)) {
                     console.log(`The remaining amount is ${user.balance - Number(amountToBeWithDraw.amount)}`);
+                }
+                else {
+                    console.log(chalk.bgRed("You have in sufficient balance."));
                 }
             }
         }
